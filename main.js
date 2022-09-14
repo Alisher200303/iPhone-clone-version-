5c81e5ae3d63a6b8477d015d48956e3d130824a6
Alisher <alishertogoyev682@gmail.com>
2022-09-14 20:32:03 +0500

    iPhone-clone-3

--- main.js
+++ main.js
@@ -2,14 +2,17 @@
 
 
 const btn = document.querySelectorAll("div"),
-      display = document.querySelector(".first-display"),
-      display2 = document.querySelector(".phone-line"),
-      secondScreen = document.querySelector("#secondScreen"),
-      thirdScreen = document.querySelector("#thirdScreen"),
-      fourthdScreen = document.querySelector("#fourthdScreen"),
-      callPhone = document.querySelector("#callPhone"),
-      exitBtn = document.querySelector(".exit-btn");
-    
+    display = document.querySelector(".first-display"),
+    display2 = document.querySelector(".phone-line"),
+    secondScreen = document.querySelector("#secondScreen"),
+    thirdScreen = document.querySelector("#thirdScreen"),
+    fourthdScreen = document.querySelector("#fourthdScreen"),
+    callPhone = document.querySelector("#callPhone"),
+    exitBtn = document.querySelector(".exit-btn"),
+    hiddenIcon = document.querySelectorAll(".bi-info-circle"),
+    editBtn = document.querySelector("#sort-btn"),
+    changeDate = document.querySelectorAll(".date-change");
+
 
 btn.forEach(item => {
     item.addEventListener("click", () => {
@@ -27,5 +30,47 @@ btn.forEach(item => {
         }
     });
 });
+const iconRemove = document.querySelectorAll("#iconChange");
 
 
+editBtn.addEventListener("click", () => {
+    if (editBtn.innerHTML = "Edit") {
+        editBtn.innerHTML = "Tink";
+        changeDate.forEach(item => {
+            gsap.to(item, {
+                x: 20,
+                duration: 0.4,
+            });
+        });
+        hiddenIcon.forEach(hide => {
+            gsap.to(hide, {
+                scale: 0,
+            });
+        });
+    }
+    editBtn.addEventListener("click", () => {
+        if (editBtn.innerHTML = "Tink") {
+            editBtn.innerHTML = "Edit";
+            changeDate.forEach(item => {
+                gsap.to(item, {
+                    x: 0,
+                    duration: 0.4,
+                });
+            });
+            hiddenIcon.forEach(hide => {
+                gsap.to(hide, {
+                    scale: 1,
+                });
+            }); 
+        }
+    });
+});
+
+
+
+// <i class="bi bi-dash-circle-fill"></i>
+
+// iconRemove.forEach(change => {
+//     change.classList.remove("bi-telephon-inbound-fill");
+//     change.classList.add("bi-dash-circle-fill");
+// });
